2551d7f8cfd83f3c3505fcf94ba231d813b1f50b
Kihun SONG <kihunbuzz@naver.com>
2023-03-14 01:19:17 +0900

    [FE] feat : Aurora Fragment With Google Map

--- client/Aro/app/src/main/java/com/nassafy/aro/UI/View/aurora/AuroraFragment.kt
+++ client/Aro/app/src/main/java/com/nassafy/aro/UI/View/aurora/AuroraFragment.kt
@@ -1,11 +1,45 @@
 package com.nassafy.aro.UI.View.aurora
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import com.nassafy.aro.R
+import com.nassafy.aro.databinding.FragmentAuroraBinding
 
-class AuroraFragment : Fragment() {
+private const val TAG = "AuroraFragment_sdr"
+class AuroraFragment : Fragment(), OnMapReadyCallback  {
+    private var _binding: FragmentAuroraBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var googleMap: GoogleMap
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentAuroraBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map_view) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
+    }
+
+    override fun onMapReady(gMap: GoogleMap) {
+        googleMap = gMap
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
