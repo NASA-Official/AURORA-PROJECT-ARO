d33db34c6d93762063afe64875a298cc277468ff
Kihun SONG <kihunbuzz@naver.com>
2023-03-13 17:57:11 +0900

    [FE] feat : TabLayout and Fragment Ongoing

---
+++ client/Aro/app/src/main/java/com/nassafy/aro/UI/View/aurora/AuroraFragment.kt
@@ -0,0 +1,11 @@
+package com.nassafy.aro.UI.View.aurora
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.nassafy.aro.R
+
+class AuroraFragment : Fragment() {
+}
--- client/Aro/app/src/main/java/com/nassafy/aro/UI/View/main/MainFragment.kt
+++ client/Aro/app/src/main/java/com/nassafy/aro/UI/View/main/MainFragment.kt
@@ -1,23 +1,76 @@
 package com.nassafy.aro.UI.view.main
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 import com.nassafy.aro.R
+import com.nassafy.aro.UI.View.aurora.AuroraFragment
+import com.nassafy.aro.UI.View.TempFragment
+import com.nassafy.aro.databinding.FragmentMainBinding
 
+private const val TAG = "MainFragment_sdr"
 class MainFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+    private var tabTitle : ArrayList<String> = arrayListOf()
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.d(TAG, "onAttach: hi")
 
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+    ): View {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.viewpager.apply {
+            adapter = ViewPagerAdapter(childFragmentManager, lifecycle)
+            isUserInputEnabled = false
+        }
+
+        // add tablayout title
+        tabTitle = arrayListOf(getString(R.string.aurora), getString(R.string.meteor_shower))
+
+        TabLayoutMediator(binding.tablayout, binding.viewpager) { tab, position ->
+            tab.text = tabTitle[position]
+        }.attach()
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+    inner class ViewPagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
+        FragmentStateAdapter(fragmentManager, lifecycle) {
+        override fun getItemCount(): Int {
+            return tabTitle.size
+        }
+
+        override fun createFragment(position: Int): Fragment {
+            return when(position) {
+                0 -> AuroraFragment()
+                // TODO: Meteor Shower Fragment
+                else -> TempFragment()
+            }
+        }
+    }
 }
