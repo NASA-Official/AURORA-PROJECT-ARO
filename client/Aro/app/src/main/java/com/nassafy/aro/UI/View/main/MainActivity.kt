bc0f0b39c63ed4f786679d32eddf5985d6c329e8
bigyoung8375 <bigyoung8375@naver.com>
2023-03-15 00:24:14 +0900

    [FE] Feat : Country Stamp customview & imageFragment Add

--- client/Aro/app/src/main/java/com/nassafy/aro/UI/View/main/MainActivity.kt
+++ client/Aro/app/src/main/java/com/nassafy/aro/UI/View/main/MainActivity.kt
@@ -1,12 +1,23 @@
-package com.nassafy.aro.UI.view.main
+package com.nassafy.aro.ui.view.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.nassafy.aro.R
+import com.nassafy.aro.ui.view.stamp.CountryPlaceViewPagerAdapter
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var countryPlaceAdapter: CountryPlaceViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        countryPlaceAdapter = CountryPlaceViewPagerAdapter(this)
+        hideStatusBar()
     }
+
+    private fun hideStatusBar() {
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+    } // End of hideStatusBar
 }
