dcd4eb49e485bd69be1e213e74b0efeb8c9fd95b
Chanseok <ckstjr93@naver.com>
2023-03-14 10:43:25 +0900

    [BE] init

---
+++ server/aro/src/main/java/com/nassafy/aro/AroApplication.java
@@ -0,0 +1,13 @@
+package com.nassafy.aro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AroApplication.class, args);
+	}
+
+}
---
+++ server/aro/src/test/java/com/nassafy/aro/AroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nassafy.aro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
