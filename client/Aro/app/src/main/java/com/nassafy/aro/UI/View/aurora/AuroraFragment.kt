5457371ec4c78c7b298df88800f353b509f98ffc
Kihun SONG <kihunbuzz@naver.com>
2023-03-15 13:18:30 +0900

    [FE] Custom App Bar with Drawer Layout

--- client/Aro/app/src/main/java/com/nassafy/aro/UI/View/aurora/AuroraFragment.kt
+++ client/Aro/app/src/main/java/com/nassafy/aro/UI/View/aurora/AuroraFragment.kt
@@ -5,11 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.nassafy.aro.R
 import com.nassafy.aro.databinding.FragmentAuroraBinding
+import com.nassafy.aro.ui.view.main.MainActivity
 
 private const val TAG = "AuroraFragment_sdr"
 class AuroraFragment : Fragment(), OnMapReadyCallback  {
@@ -30,6 +32,16 @@ class AuroraFragment : Fragment(), OnMapReadyCallback  {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map_view) as? SupportMapFragment
         mapFragment?.getMapAsync(this)
+
+        binding.drawerImagebutton.setOnClickListener {
+            val mainActivity = activity as MainActivity
+            mainActivity.openDrawer()
+        }
+
+        binding.dateTimeLinearlayout.setOnClickListener {
+            Toast.makeText(context, "dateTime", Toast.LENGTH_SHORT).show()
+        }
+
     }
 
     override fun onMapReady(gMap: GoogleMap) {
