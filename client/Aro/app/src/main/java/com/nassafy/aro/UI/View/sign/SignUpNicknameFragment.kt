457e5bcc8f75bf5b34a52dbfc92e89562fbf8d0e
박채성 <97psh@naver.com>
2023-03-15 16:38:54 +0900

    Merge Completed

---
+++ client/Aro/app/src/main/java/com/nassafy/aro/UI/View/sign/SignUpNicknameFragment.kt
@@ -0,0 +1,53 @@
+package com.nassafy.aro.ui.view.sign
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.nassafy.aro.R
+import com.nassafy.aro.databinding.FragmentSignUpNicknameBinding
+
+class SignUpNicknameFragment : Fragment() {
+
+
+    private var _binding: FragmentSignUpNicknameBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSignUpNicknameBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initView()
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun initView() {
+
+        binding.nextButton.setOnClickListener {
+            findNavController().navigate(R.id.action_signUpNicknameFragment_to_signUpServiceFragment)
+        }
+        binding.cancelButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
+    }
+
+}
---
+++ client/Aro/app/src/main/java/com/nassafy/aro/data/repository/TestRepository.kt
@@ -0,0 +1,8 @@
+package com.nassafy.aro.data.repository
+
+import com.nassafy.aro.domain.repository.api.TestApi
+import javax.inject.Inject
+
+class TestRepository @Inject constructor(private val testApi: TestApi) {
+
+}
