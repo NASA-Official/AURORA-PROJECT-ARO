c3db99fe8c1637a1b6f52b7a2d5bf6e7cb8cdeec
Chanseok <ckstjr93@naver.com>
2023-03-17 17:56:16 +0900

    [BE] ADD Log

--- server/aro/api-module/src/main/java/com/nassafy/api/service/CustomUserDetailsService.java
+++ server/aro/api-module/src/main/java/com/nassafy/api/service/CustomUserDetailsService.java
@@ -1,8 +1,11 @@
 package com.nassafy.api.service;
 
+import com.nassafy.api.jwt.JwtAuthenticationFilter;
 import com.nassafy.core.entity.Member;
 import com.nassafy.core.respository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,11 +16,14 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
+
+    private static final Logger logger = LoggerFactory.getLogger(CustomUserDetailsService.class);
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        logger.debug("\t Start loadUserByUsername");
         return memberRepository.findByMemberId(username)
                 .map(this::createUserDetails)
                 .orElseThrow(() -> new UsernameNotFoundException("해당하는 유저를 찾을 수 없습니다."));
@@ -25,6 +31,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     // 해당하는 User 의 데이터가 존재한다면 UserDetails 객체로 만들어서 리턴
     private UserDetails createUserDetails(Member member) {
+        logger.debug("\t Start createUserDetails");
         return User.builder()
                 .username(member.getUsername())
                 .password(passwordEncoder.encode(member.getPassword()))
