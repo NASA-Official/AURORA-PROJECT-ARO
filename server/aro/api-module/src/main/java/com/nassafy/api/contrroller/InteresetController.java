bae7b668ec6ef690ed57b2bf29feca883c03d7a7
고청천 <g2c1010@naver.com>
2023-03-18 22:56:30 +0900

    [BE] Fix: Api 오류 수정

--- server/aro/api-module/src/main/java/com/nassafy/api/contrroller/InteresetController.java
+++ server/aro/api-module/src/main/java/com/nassafy/api/contrroller/InteresetController.java
@@ -5,18 +5,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/aurora")
+@RequestMapping("/api/aurora")
 @RequiredArgsConstructor
 public class InteresetController {
     private final InterestService interestService;
 
     //관심 지역 등록 테스트 해봐야함
     @PostMapping("/favorite")
-    public ResponseEntity<String> registerInterest(@RequestParam Long memberId, @RequestBody List<Long> attractionIds) {
+    public ResponseEntity<String> registerInterest(@RequestParam Long memberId, @RequestBody Map<String, Object> requestBody) {
+        List<Integer> ids = (List<Integer>) requestBody.get("attractionIds");
+        List<Long> attractionIds = ids.stream()
+                .map(Long::valueOf)
+                .collect(Collectors.toList());
         interestService.registerInterest(memberId, attractionIds);
         return ResponseEntity.ok("success");
     }
+
+
 
 }
