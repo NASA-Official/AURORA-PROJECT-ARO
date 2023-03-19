ea29984f424199ab245c8590a5494f66c3e93008
諛뺤콈ì꽦 <97psh@naver.com>
2023-03-19 17:20:46 +0900

    [FE] Feat : Set github Oauth2

--- client/Aro/app/src/main/java/com/nassafy/aro/UI/View/sign/SignInFragment.kt
+++ client/Aro/app/src/main/java/com/nassafy/aro/UI/View/sign/SignInFragment.kt
@@ -1,5 +1,7 @@
 package com.nassafy.aro.ui.view.sign
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -7,8 +9,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.nassafy.aro.BuildConfig.GITHUB_CLIENT_ID
 import com.nassafy.aro.R
 import com.nassafy.aro.databinding.FragmentSignInBinding
+import com.nassafy.aro.util.githubLoginUri
 
 class SignInFragment : Fragment() {
 
@@ -39,10 +43,20 @@ class SignInFragment : Fragment() {
         binding.signUpTextview.setOnClickListener {
             findNavController().navigate(R.id.action_signInFragment_to_signUpEmailFragment2)
         }
-    }
+        binding.signInGithubImagebutton.setOnClickListener {
+            githubLogin()
+        }
+    } // ENd of initView
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+    fun githubLogin() {
+        startActivity(Intent(Intent.ACTION_VIEW, githubLoginUri).apply {
+            addCategory(Intent.CATEGORY_BROWSABLE)
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        })
+    } // End of githubLogin
 }
