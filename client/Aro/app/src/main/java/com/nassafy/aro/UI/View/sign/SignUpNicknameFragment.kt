442087117e2f24983c0f83d30d0097b43f0a53f0
박채성 <97psh@naver.com>
2023-03-19 19:50:36 +0900

    [FE] Feat : Add Textfield Error Msg

--- client/Aro/app/src/main/java/com/nassafy/aro/UI/View/sign/SignUpNicknameFragment.kt
+++ client/Aro/app/src/main/java/com/nassafy/aro/UI/View/sign/SignUpNicknameFragment.kt
@@ -1,6 +1,8 @@
 package com.nassafy.aro.ui.view.sign
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -48,7 +50,22 @@ class SignUpNicknameFragment : Fragment() {
         binding.cancelButton.setOnClickListener {
             findNavController().popBackStack()
         }
-
+        binding.signUpNicknameIdTextfield.apply {
+            editText?.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+                override fun onTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+                override fun afterTextChanged(text: Editable?) {
+                    when (text.toString().length in 3..10) {
+                        true -> {
+                            error = ""
+                        } // End of case true
+                        false -> {
+                            error = getString(R.string.password_validate_fail_textview_text)
+                        } // End of case false
+                    } // End of when
+                } // End of afterTextChanged
+            }) // End of addTextChangedListener
+        }
     }
 
 }
