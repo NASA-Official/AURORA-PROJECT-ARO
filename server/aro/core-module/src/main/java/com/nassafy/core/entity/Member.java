142f96262cd098d81ef03011b649a53b12d471a6
怨좎껌泥 <g2c1010@naver.com>
2023-03-20 22:12:29 +0900

    Revert "Merge branch 'feature/be/stamp' into 'develop_be'"
    This reverts merge request !11

--- server/aro/core-module/src/main/java/com/nassafy/core/entity/Member.java
+++ server/aro/core-module/src/main/java/com/nassafy/core/entity/Member.java
@@ -1,46 +1,74 @@
 package com.nassafy.core.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
-@Getter @Setter
-public class Member {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "member_id")
-    private Long id;
+public class Member implements UserDetails {
 
+    @Id
+    @Column(updatable = false, unique = true, nullable = false)
     private String email;
 
+    @Column(nullable = false)
     private String password;
 
+    @Column(nullable = false)
     private String nickname;
 
-    private boolean alarm = true;
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Builder.Default
+    private List<String> roles = new ArrayList<>();
 
-    private boolean auroraDisplay = true;
-
-    private boolean auroraService = false;
-    @JsonIgnore
-    @OneToMany(mappedBy = "member")
-    private List<Interest> interests = new ArrayList<>();
-
-    @OneToMany(mappedBy = "member")
-    private List<Stamp> stamps = new ArrayList<>();
-
-    public void toggleAlarm(){
-        this.alarm = !this.alarm;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
     }
 
-    public void toggleAuroraDisplay(){
-        this.auroraDisplay = !this.auroraDisplay;
+    @Override
+    public String getUsername() {
+        return email;
     }
 
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
-
