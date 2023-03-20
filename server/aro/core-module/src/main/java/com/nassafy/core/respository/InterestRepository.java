e237421d50602db08fbeff1f70017431b65fe182
怨좎껌泥 <g2c1010@naver.com>
2023-03-20 17:35:07 +0900

    [BE] Fix: 브랜치 되돌리기

--- server/aro/core-module/src/main/java/com/nassafy/core/respository/InterestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nassafy.core.respository;
-
-import com.nassafy.core.entity.Interest;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InterestRepository extends JpaRepository<Interest, Long> {
-}
