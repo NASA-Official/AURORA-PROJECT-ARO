759b97589391b506e6fa3c3ad61bc253f0cfd472
고청천 <g2c1010@naver.com>
2023-03-20 22:12:29 +0900

    Revert "Merge branch 'feature/be/stamp' into 'develop_be'"
    This reverts merge request !11

--- server/aro/api-module/src/main/java/com/nassafy/api/controller/TestController.java
+++ server/aro/api-module/src/main/java/com/nassafy/api/controller/TestController.java
@@ -1,7 +1,6 @@
 package com.nassafy.api.controller;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-
-    @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping("/")
     public ResponseEntity<String> test() {
-        String responseJson = "{ \"message\": \"test\" }"; // JSON 형식의 문자열 반환
-        return ResponseEntity.ok(responseJson);
+        return new ResponseEntity<>("test", HttpStatus.OK);
     }
 
 }
--- server/aro/core-module/src/main/java/com/nassafy/core/respository/MemberRepository.java
+++ server/aro/core-module/src/main/java/com/nassafy/core/respository/MemberRepository.java
@@ -1,14 +1,10 @@
 package com.nassafy.core.respository;
 
 import com.nassafy.core.entity.Member;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
-    Member findByEmail(String email);
-
-    Optional<Member> findById(Long id);
+public interface MemberRepository extends JpaRepository<Member, String> {
+    Optional<Member> findByEmail(String username);
 }
