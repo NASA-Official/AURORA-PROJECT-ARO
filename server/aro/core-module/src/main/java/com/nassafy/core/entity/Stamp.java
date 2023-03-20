36dfc58f4ca656f73da4c08fa4275c922d2bb953
怨좎껌泥 <g2c1010@naver.com>
2023-03-20 12:35:37 +0900

    [Feat] Stamp 엔티티 추가 및 엔티티 수정

---
+++ server/aro/core-module/src/main/java/com/nassafy/core/entity/Stamp.java
@@ -0,0 +1,29 @@
+package com.nassafy.core.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class Stamp {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "stamp_id")
+    private Long id;
+
+    private Boolean certification = false;
+
+    private String memo;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "attraction_id")
+    private Attraction attraction;
+}
