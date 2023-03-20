14f89a9b2c54cf2a2a7fc24dacaf3f3f87bad672
Kihun SONG <kihunbuzz@naver.com>
2023-03-20 17:46:32 +0900

    [FE] Fix : MapView Error Issue

---
+++ client/Aro/app/src/main/java/com/nassafy/aro/ui/view/aurora/AuroraViewModel.kt
@@ -0,0 +1,17 @@
+package com.nassafy.aro.ui.view.aurora
+
+import androidx.lifecycle.ViewModel
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.nassafy.aro.R
+
+class AuroraViewModel : ViewModel() {
+//    private lateinit var googleMap: GoogleMap
+
+//    fun initMap() {
+//        val mapFragment: SupportMapFragment? =
+//            childFragmentManager.findFragmentById(R.id.map_view) as? SupportMapFragment
+//        googleMap = mapFragment?.awaitMap()!!
+//    }
+
+}
