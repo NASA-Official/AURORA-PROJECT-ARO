31950631c808c10e536c9c6548cfb0c074c889c5
bigyoung8375@naver.com <bigyoung8375@naver.com>
2023-03-20 14:44:42 +0900

    [FE] Feat : Main Navigation modify

--- client/Aro/app/src/main/java/com/nassafy/aro/UI/View/main/MainActivity.kt
+++ client/Aro/app/src/main/java/com/nassafy/aro/UI/View/main/MainActivity.kt
@@ -1,22 +1,15 @@
 package com.nassafy.aro.ui.view.main
 
 import android.os.Bundle
-import android.view.MenuItem
 import android.view.View
 import android.widget.ImageButton
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.NavController
-import androidx.navigation.NavHostController
-import androidx.navigation.Navigation
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.navigation.NavigationView
 import com.nassafy.aro.R
 import com.nassafy.aro.databinding.ActivityMainBinding
 
