9e66ff80fa58f1e44a9d78b2a29b63e2df9c2ed1
Chanseok <ckstjr93@naver.com>
2023-03-21 15:33:59 +0900

    [BE] Dev: mail Auth

--- server/aro/api-module/src/main/java/com/nassafy/api/controller/AccountController.java
+++ server/aro/api-module/src/main/java/com/nassafy/api/controller/AccountController.java
@@ -1,7 +1,10 @@
 package com.nassafy.api.controller;
 
+import com.nassafy.api.dto.req.CodeCheckDto;
+import com.nassafy.api.dto.req.EmailCheckDto;
 import com.nassafy.api.dto.req.SignupReqDto;
 import com.nassafy.api.dto.res.SignupResDto;
+import com.nassafy.api.service.EmailService;
 import com.nassafy.api.service.MemberService;
 import com.nassafy.core.respository.MemberRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +15,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Email;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @Slf4j
@@ -21,28 +27,40 @@ import java.util.Optional;
 public class AccountController {
     private static final Logger logger = LoggerFactory.getLogger(AccountController.class);
     private final MemberService memberService;
+    private final EmailService emailService;
     private final MemberRepository memberRepository;
 
     private final int mailCode = 123456;
 
-    @PostMapping("/emailcheck/{email}")
-    public ResponseEntity<?> emailCheck(@PathVariable("email") String email) {
+    private Map<String, String> emailCode = new HashMap<>();
+
+    @PostMapping("/emailcheck")
+    public ResponseEntity<?> emailCheck(@RequestBody EmailCheckDto emailCheckDto) throws Exception {
+        String email = emailCheckDto.getEmail();
         logger.debug("\t Start emailCheck " + email);
         if(memberRepository.findByEmail(email).isPresent()){
             return ResponseEntity.badRequest().body("Error: email is already used!!");
         }
 
+        String code = emailService.sendSimpleMessage(email);
+        logger.debug("\t code " + code);
+        emailCode.put(email, code);
         return ResponseEntity.ok("email is not used!");
     }
 
-    @PostMapping("/codecheck/{code}")
-    public ResponseEntity<?> codeCheck(@PathVariable("code") int code) {
-        logger.debug("\t Start codeCheck " + code);
-        System.out.println(code);
-        if(code != mailCode){
+    @PostMapping("/codecheck")
+    public ResponseEntity<?> codeCheck(@RequestBody CodeCheckDto codeCheckDto) {
+        String email = codeCheckDto.getEmail();
+        String code = codeCheckDto.getCode();
+        logger.debug("\t Start codeCheck " + email + ", " + code);
+
+        if(!emailCode.containsKey(email)){
+            return ResponseEntity.badRequest().body("Error: email is not checked!!");
+        }
+        if(!code.equals(emailCode.get(email))){
             return ResponseEntity.badRequest().body("Error: code is different!!");
         }
-
+        emailCode.remove(email);
         return ResponseEntity.ok("code is same!!!");
     }
 
