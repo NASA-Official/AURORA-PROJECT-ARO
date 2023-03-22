c48b58533d0a8f8b6fb9a752155ca0c12f9b2e61
unknown <gagopa9912@naver.com>
2023-03-23 01:26:12 +0900

    [BE] diary create 삭제 및 diary edit 수정

--- server/aro/api-module/src/main/java/com/nassafy/api/controller/StampController.java
+++ server/aro/api-module/src/main/java/com/nassafy/api/controller/StampController.java
@@ -1,22 +1,18 @@
 package com.nassafy.api.controller;
 
 
-import com.nassafy.api.dto.req.SingupAttractionDTO;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nassafy.api.dto.req.SingupAttractionDTO;
 import com.nassafy.api.dto.req.StampDiaryReqDTO;
 import com.nassafy.api.dto.res.StampDiaryResDTO;
+import com.nassafy.api.service.JwtService;
 import com.nassafy.api.service.StampService;
 import com.nassafy.core.DTO.MapStampDTO;
-import com.nassafy.core.DTO.RegisterStampDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -30,67 +26,66 @@ public class StampController {
     @Autowired
     private StampService stampService;
 
+    @Autowired
+    private JwtService jwtService;
+
     @GetMapping("{memberId}/{nation}")
     public ResponseEntity<List<MapStampDTO>> getStampMemberAndCountry(@PathVariable Long memberId, @PathVariable String nation){
         List<MapStampDTO> mapStamps = stampService.findStampsByUserAndCountry(memberId, nation);
         return ResponseEntity.ok(mapStamps);
     }
 
-    @PostMapping(value = "diary/{nation}/{attraction}/{memberId}",
-            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
-    public ResponseEntity<?> createStampDiary (
-            @RequestPart("files") List<MultipartFile> files,
-            @RequestPart("memo") String memo,
-            @PathVariable String nation,
-            @PathVariable String attraction,
-            @PathVariable Long memberId) {
+//    @PostMapping(value = "diary/{nation}/{attraction}/{memberId}",
+//            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+//    public ResponseEntity<?> createStampDiary (
+//            @RequestPart("files") List<MultipartFile> files,
+//            @RequestPart("memo") String memo,
+//            @PathVariable String nation,
+//            @PathVariable String attraction,
+//            @PathVariable Long memberId) {
+//
+//        log.info("start create stamp diary");
+//        log.info("memo:" + memo);
+//        log.info("file size: " + files.size());
+//        log.info(String.valueOf(files.getClass()));
+//
+//        try {
+//            stampService.createStampDiary(nation, attraction, memberId,
+//                    StampDiaryReqDTO.builder().memo(memo).files(files).build());
+//            return new ResponseEntity<>(HttpStatus.CREATED);
+//        } catch (IllegalArgumentException | IOException e) {
+//            log.debug(e.getMessage());
+//            log.debug("BAD REQUEST");
+//            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+//        }
+//    }
 
-        log.info("start create stamp diary");
-        log.info("memo:" + memo);
-        log.info("file size: " + files.size());
-        log.info(String.valueOf(files.getClass()));
-
-        try {
-            stampService.createStampDiary(nation, attraction, memberId,
-                    StampDiaryReqDTO.builder().memo(memo).files(files).build());
-            return new ResponseEntity<>(HttpStatus.CREATED);
-        } catch (IllegalArgumentException | IOException e) {
-            log.debug(e.getMessage());
-            log.debug("BAD REQUEST");
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-    }
-
-    @PutMapping(value = "diary/{nation}/{attraction}/{memberId}",
+    @PostMapping(value = "diary/{attractionId}",
             consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
     public ResponseEntity<?> editStampDiary (
-            @RequestPart("newImageLists") List<MultipartFile> newImageLists,
-            @RequestPart("deleteImageLists") String deleteImageLists,
-            @RequestPart("memo") String memo,
-            @PathVariable String nation,
-            @PathVariable String attraction,
-            @PathVariable Long memberId) throws JsonProcessingException {
+            @RequestPart("newImageList") List<MultipartFile> newImageList,
+            @RequestPart("data") StampDiaryReqDTO stampDiaryReqDTO,
+            @PathVariable Long attractionId) {
+        log.info("start edit diary");
 
-        ObjectMapper mapper = new ObjectMapper();
-        List<String> deleteImageListsObject = mapper.readValue(deleteImageLists, new TypeReference<List<String>>() {
-        });
+        String email = jwtService.getUserEmailFromJwt();
 
         try {
-            stampService.editStampDiary(nation, attraction, memberId, newImageLists, deleteImageListsObject, memo);
+            stampService.editStampDiary(email, attractionId, newImageList, stampDiaryReqDTO);
             return new ResponseEntity<>(HttpStatus.OK);
         } catch (IllegalArgumentException | IOException e) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
     }
 
-    @GetMapping(value = "diary/{nation}/{attraction}/{memberId}")
+    @GetMapping(value = "diary/{attractionId}")
     public ResponseEntity<?> getStampDiary (
-            @PathVariable String nation,
-            @PathVariable String attraction,
-            @PathVariable Long memberId) {
+            @PathVariable Long attractionId) {
+
+        String email = jwtService.getUserEmailFromJwt();
 
         try {
-            StampDiaryResDTO result = stampService.getStampDiary(nation, attraction, memberId);
+            StampDiaryResDTO result = stampService.getStampDiary(attractionId, email);
             return new ResponseEntity<>(result,HttpStatus.OK);
         } catch (IllegalArgumentException e) {
             log.debug(e.getMessage());
