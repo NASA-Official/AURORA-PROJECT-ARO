1db15a01d53de684b3079d2b321722326cba44cc
Kihun SONG <kihunbuzz@naver.com>
2023-03-24 01:07:48 +0900

    [FE] Feat : Custom Marker, Map Clustering

---
+++ client/Aro/app/src/main/java/com/nassafy/aro/ui/view/CustomMarkerRenderer.kt
@@ -0,0 +1,26 @@
+package com.nassafy.aro.ui.view
+
+import android.content.Context
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.maps.android.clustering.ClusterManager
+import com.google.maps.android.clustering.view.DefaultClusterRenderer
+import com.nassafy.aro.R
+import com.nassafy.aro.data.dto.Place
+import com.nassafy.aro.util.generateBitmapDescriptorFromRes
+
+class CustomMarkerRenderer(
+    context: Context, googleMap: GoogleMap, clusterManager: ClusterManager<Place>) :
+    DefaultClusterRenderer<Place>(context, googleMap, clusterManager) {
+
+    private val customMarker = generateBitmapDescriptorFromRes(context, R.drawable.map_marker)
+
+
+    override fun onBeforeClusterItemRendered(item: Place, markerOptions: MarkerOptions) {
+        markerOptions
+            .position(item.position)
+            .title(item.title)
+            .icon(customMarker)
+        super.onBeforeClusterItemRendered(item, markerOptions)
+    }
+}
