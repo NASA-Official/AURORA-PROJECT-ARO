87b05fabf231cf5e7e55b380028596ba3b106226
Kihun SONG <kihunbuzz@naver.com>
2023-03-23 13:05:45 +0900

    [FE] Feat : Bottom Sheet Chart

---
+++ client/Aro/app/src/main/java/com/nassafy/aro/ui/view/ChartMarkerView.kt
@@ -0,0 +1,36 @@
+package com.nassafy.aro.ui.view
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.Log
+import android.widget.TextView
+import com.github.mikephil.charting.components.MarkerView
+import com.github.mikephil.charting.data.CandleEntry
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.highlight.Highlight
+import com.github.mikephil.charting.utils.MPPointF
+import com.github.mikephil.charting.utils.Utils
+import com.nassafy.aro.R
+
+@SuppressLint("ViewConstructor")
+private const val TAG = "ChartMarkerView_SDR"
+class ChartMarkerView(context: Context, layoutResource: Int) : MarkerView(context, layoutResource) {
+
+    private var chartMarkerTextview: TextView = findViewById(R.id.chart_marker_textview)
+
+    // runs every time the MarkerView is redrawn, can be used to update the
+    // content (user-interface)
+    override fun refreshContent(e: Entry, highlight: Highlight?) {
+        if (e is CandleEntry) {
+            chartMarkerTextview!!.text = Utils.formatNumber(e.high, 0, true)
+        } else {
+            Log.d(TAG, "refreshContent: ${e.y}")
+            chartMarkerTextview!!.text = "${e.y}"
+        }
+        super.refreshContent(e, highlight)
+    }
+
+    override fun getOffset(): MPPointF {
+        return MPPointF(-(width / 2).toFloat(), -height.toFloat())
+    }
+}
