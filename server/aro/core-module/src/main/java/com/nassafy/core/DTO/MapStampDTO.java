89244d019d9697aa14f35985c83d1d6aacff8bc0
고청천 <g2c1010@naver.com>
2023-03-24 00:31:28 +0900

    [BE] 22Api 수정 23Api 개발

--- server/aro/core-module/src/main/java/com/nassafy/core/DTO/MapStampDTO.java
+++ server/aro/core-module/src/main/java/com/nassafy/core/DTO/MapStampDTO.java
@@ -4,10 +4,15 @@ import lombok.Getter;
 
 @Getter
 public class MapStampDTO {
+
+    private Long attractionId;
     private String colorStamp;
     private boolean certification;
 
-    public MapStampDTO(String colorStamp, boolean certification) {
+
+
+    public MapStampDTO(Long attractionId ,String colorStamp, boolean certification) {
+        this.attractionId = attractionId;
         this.colorStamp = colorStamp;
         this.certification = certification;
     }
