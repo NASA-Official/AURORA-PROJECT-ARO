3e9930f745a90a99989fe86adc8418e212c8422d
諛뺤콈ì꽦 <97psh@naver.com>
2023-03-29 15:51:24 +0900

    [AI] Feat : Finish develop the aurora detection server
    Former-commit-id: f09df39b5c6fc41be31669d23e0e6b704935bfa2

--- server/aroAi/aroAiServer.py
+++ server/aroAi/aroAiServer.py
@@ -36,9 +36,13 @@ from pathlib import Path
 
 import torch
 
+
 import json
-from flask import Flask
+from flask import Flask, Response, request, abort
 from flask_cors import CORS, cross_origin
+from io import BytesIO
+
+import numpy as np
 
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0]  # YOLOv5 root directory
@@ -47,7 +51,7 @@ if str(ROOT) not in sys.path:
 ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
 from models.common import DetectMultiBackend
-from utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadImages, LoadScreenshots, LoadStreams
+from utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadScreenshots, LoadStreams
 from utils.general import (LOGGER, Profile, check_file, check_img_size, check_imshow, check_requirements, colorstr, cv2,
                            increment_path, non_max_suppression, print_args, scale_boxes, strip_optimizer, xyxy2xywh)
 from utils.plots import Annotator, colors, save_one_box
@@ -60,59 +64,38 @@ cors = CORS(app, resources={
     }
 })
 
-# @app.route('/file', methods=['POST'])
-# def getFile(fle=None):
-#     if request.method == 'POST':
-#         if 'file' not in request.files:
-#             return 'File is missing', 404
-#
-#         pic_data = request.files['file']
-#         filename = secure_filename(pic_data.filename)  # 업로드 된 파일의 이름이 안전한가를 확인해주는 함수이다. 해킹 공격에 대해 보안을 하고자 사용되기도 한다.
-#         print(pic_data)
-#         print(filename)
-#     return 'ok'
-
-
-@app.route('/certify', methods=['POST'])
+@app.route('/flask/validate', methods=['POST'])
 def getImage():
-    # request.files 에서 파일 가져오기    file = request.files['image'].read()
+    # request.files 에서 파일 가져오기    
+    file = request.files['image'].read()
 
     # 파일 없을 겨우
-    if not file:
-        abort(400, 'No file uploaded')
+    #if not file:
+    #    abort(415, 'No file uploaded')
 
     result = certifyAurora(file)
+    if result == True:
+        return Response(status=201, mimetype='application/json')
+    else :
+        return abort(400)
 
-    return '{"result": {0} }'.format(result)
-    
+
 @smart_inference_mode()
 def certifyAurora(image_data):
-    weights='./weights/aurora.pt'
+    weights = './weights/aurora.pt'
     data = './aurora.yaml'
     imgsz = (640, 640)
     conf_thres = 0.25  # confidence threshold
-    iou_thres=0.45  # NMS IOU threshold
+    iou_thres = 0.45  # NMS IOU threshold
     max_det = 1000  # maximum detections per image
     device = ''  # cuda device, i.e. 0 or 0,1,2,3 or cpu
-    view_img=False  # show results
-    save_txt=False  # save results to *.txt
-    save_conf=False  # save confidences in --save-txt labels
-    save_crop=False  # save cropped prediction boxes
-    nosave=False  # do not save images/videos
-    classes=None  # filter by class: --class 0, or --class 0 2 3
-    agnostic_nms=False  # class-agnostic NMS
-    augment=False  # augmented inference
-    visualize=False  # visualize features
-    update=False  # update all models
-    project='./runs/detect',  # save results to project/name
-    name='exp'  # save results to project/name
-    exist_ok=False  # existing project/name ok, do not increment
-    line_thickness=3  # bounding box thickness (pixels)
-    hide_labels=False  # hide labels
-    hide_conf=False  # hide confidences
-    half=False  # use FP16 half-precision inference
-    dnn=False  # use OpenCV DNN for ONNX inference
-    vid_stride=1  # video frame-rate stride
+    classes = None  # filter by class: --class 0, or --class 0 2 3
+    agnostic_nms = False  # class-agnostic NMS
+    augment = False  # augmented inference
+    visualize = False  # visualize features
+    half = False  # use FP16 half-precision inference
+    dnn = False  # use OpenCV DNN for ONNX inference
+    vid_stride = 1  # video frame-rate stride
 
     device = select_device(device)
     model = DetectMultiBackend(weights, device=device, dnn=dnn, data=data, fp16=half)
@@ -120,41 +103,37 @@ def certifyAurora(image_data):
 
     imgsz = check_img_size(imgsz, s=stride)  # check image size
 
-    bs = 1  # batch_size
-    dataset = LoadImages(image_data, img_size=imgsz, stride=stride, auto=pt, vid_stride=vid_stride)
-
-    vid_path, vid_writer = [None] * bs, [None] * bs
+    img = cv2.imdecode(np.frombuffer(image_data, dtype=np.uint8), cv2.IMREAD_COLOR)
+    img = cv2.resize(img, imgsz)
 
     model.warmup(imgsz=(1 if pt or model.triton else bs, 3, *imgsz))  # warmup
-    seen, windows, dt = 0, [], (Profile(), Profile(), Profile())
-    
-    for path, im, im0s, vid_cap, s in dataset:
-        with dt[0]:
-            im = torch.from_numpy(im).to(model.device)
-            im = im.half() if model.fp16 else im.float()  # uint8 to fp16/32
-            im /= 255  # 0 - 255 to 0.0 - 1.0
-            if len(im.shape) == 3:
-                im = im[None]  # expand for batch dim
+
+    with torch.no_grad():
+
+        im = cv2.resize(img, imgsz)
+        im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)  # RGB로 변환
+        im = im.transpose(2, 0, 1)  # 차원을 (높이, 너비, 채널)에서 (채널, 높이, 너비)로 변경
+        im = torch.from_numpy(im).unsqueeze(0).to(model.device)  # 배치 차원 추가 및 torch 텐서로 변환
+        im = im.half() if model.fp16 else im.float()  # uint8을 fp16/32로 변환
+        im /= 255  # 0 - 255를 0.0 - 1.0으로 변환
+
+        if len(im.shape) == 3:
+            im = im[None]  # expand for batch dim
 
         # Inference
-        with dt[1]:
-            visualize = increment_path(save_dir / Path(path).stem, mkdir=True) if visualize else False
-            pred = model(im, augment=augment, visualize=visualize)
+        visualize = increment_path(save_dir / Path(path).stem, mkdir=True) if visualize else False
+        pred = model(im, augment=augment, visualize=visualize)
 
         # NMS 같은 물체를 여러번 탐색하는데 그 중 하나만 선택하는 과정
-        with dt[2]:
-            pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
+        pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
 
     result = False
-    for tensor in pred:
-        for detection in tensor:
-            if detection[4] > 0.4:
-                result = True
-                print(result)
-                return result
+    for detection in pred[0]:
+        if detection[4] > 0.4:
+            result = True
+            print(result)
+            return result
     return result
 
-
 if __name__ == "__main__":
-    # certifyAurora('')
     app.run(debug=True, host='0.0.0.0', port=8080)
