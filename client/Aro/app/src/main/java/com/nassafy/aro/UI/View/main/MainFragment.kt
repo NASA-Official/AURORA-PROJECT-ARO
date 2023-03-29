6f2108462a72288b297b4785ccaa23abe66b7f17
bigyoung8375@naver.com <bigyoung8375@naver.com>
2023-03-29 13:46:24 +0900

    [FE] Feat : Permission develop

--- client/Aro/app/src/main/java/com/nassafy/aro/UI/View/main/MainFragment.kt
+++ client/Aro/app/src/main/java/com/nassafy/aro/UI/View/main/MainFragment.kt
@@ -1,27 +1,28 @@
 package com.nassafy.aro.ui.view.main
 
 import android.content.Context
-import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 import com.nassafy.aro.R
 import com.nassafy.aro.databinding.FragmentMainBinding
-import com.nassafy.aro.ui.TempFragment
 import com.nassafy.aro.ui.view.BaseFragment
 import com.nassafy.aro.ui.view.MeteorShowerFragment
 import com.nassafy.aro.ui.view.aurora.AuroraFragment
 
+private const val TAG = "MainFragment_싸피"
+
 class MainFragment : BaseFragment<FragmentMainBinding>(FragmentMainBinding::inflate) {
-    private lateinit var mContext : Context
+    private lateinit var mContext: Context
     private var tabTitle: ArrayList<String> = arrayListOf()
     private var tabIcons: ArrayList<View> = arrayListOf()
 
@@ -30,8 +31,27 @@ class MainFragment : BaseFragment<FragmentMainBinding>(FragmentMainBinding::infl
         mContext = context
     }
 
+    private fun checkPermission() {
+        TedPermission.create()
+            .setPermissionListener(object : PermissionListener {
+                override fun onPermissionGranted() {
+                    Log.d(TAG, "onPermissionGranted: 권한이 허용됨")
+
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    Log.d(TAG, "onPermissionDenied: 권한이 허용되지 않음")
+                }
+            })
+            .setDeniedMessage("갤러리 권한을 혀용해주세요")
+            .setPermissions(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            .setPermissions(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+    } // End of checkPermission
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        //checkPermission()
 
         binding.viewpager.apply {
             adapter = ViewPagerAdapter(childFragmentManager, lifecycle)
@@ -51,11 +71,15 @@ class MainFragment : BaseFragment<FragmentMainBinding>(FragmentMainBinding::infl
 
         // add tablayout Icon with using custom view
         val tabIcon1 = layoutInflater.inflate(R.layout.tab_icon_custom_view, null)
-        tabIcon1.findViewById<ImageView>(R.id.tabicon_imageview).setBackgroundResource(R.drawable.tab_aurora_icon)
-        tabIcon1.findViewById<TextView>(R.id.tabicon_textview).text = getString(R.string.service_aurora)
+        tabIcon1.findViewById<ImageView>(R.id.tabicon_imageview)
+            .setBackgroundResource(R.drawable.tab_aurora_icon)
+        tabIcon1.findViewById<TextView>(R.id.tabicon_textview).text =
+            getString(R.string.service_aurora)
         val tabIcon2 = layoutInflater.inflate(R.layout.tab_icon_custom_view, null)
-        tabIcon2.findViewById<ImageView>(R.id.tabicon_imageview).setBackgroundResource(R.drawable.tab_meteor_shower_icon)
-        tabIcon2.findViewById<TextView>(R.id.tabicon_textview).text = getString(R.string.service_meteor_shower)
+        tabIcon2.findViewById<ImageView>(R.id.tabicon_imageview)
+            .setBackgroundResource(R.drawable.tab_meteor_shower_icon)
+        tabIcon2.findViewById<TextView>(R.id.tabicon_textview).text =
+            getString(R.string.service_meteor_shower)
 
         tabIcons = arrayListOf(tabIcon1, tabIcon2)
 
