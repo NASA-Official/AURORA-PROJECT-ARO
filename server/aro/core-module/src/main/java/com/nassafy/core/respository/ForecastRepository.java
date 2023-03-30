38c1f1f769c27624d6fa34faf592f8dc5228a9e5
이민지 <gagopa9912@naver.com>
2023-03-30 11:51:11 +0900

    [BE] Feat: R batch

---
+++ server/aro/core-module/src/main/java/com/nassafy/core/respository/ForecastRepository.java
@@ -0,0 +1,13 @@
+package com.nassafy.core.respository;
+
+import com.nassafy.core.entity.Forecast;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+public interface ForecastRepository extends JpaRepository<Forecast, Integer> {
+
+    Optional<Forecast> findByDateTime(LocalDateTime dateTime);
+
+}
