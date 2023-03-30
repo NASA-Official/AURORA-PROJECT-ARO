f76bb282dd6f3270a272f06f5966b30aee01cf64
고청천 <g2c1010@naver.com>
2023-03-30 13:44:39 +0900

    [BE] Fix: 수정 반영

--- server/aro/api-module/src/main/java/com/nassafy/api/ApiApplication.java
+++ server/aro/api-module/src/main/java/com/nassafy/api/ApiApplication.java
@@ -1,6 +1,5 @@
 package com.nassafy.api;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -12,8 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableCaching
-@EnableBatchProcessing
-@ComponentScan({"com.nassafy.core", "com.nassafy.api", "com.nassafy.batch"})
+@ComponentScan({"com.nassafy.core", "com.nassafy.api"})
 @EntityScan("com.nassafy.core")
 @EnableJpaRepositories("com.nassafy.core")
 public class ApiApplication {
