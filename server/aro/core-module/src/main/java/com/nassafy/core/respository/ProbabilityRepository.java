e3eb941fb4f74bf337cb78d93c202ac84c024aa9
ì씠誘쇱§ <gagopa9912@naver.com>
2023-03-31 22:09:43 +0900

    Revert "Merge branch 'feature/be/api' into 'develop_be'"
    This reverts merge request !100

--- server/aro/core-module/src/main/java/com/nassafy/core/respository/ProbabilityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nassafy.core.respository;
-
-import com.nassafy.core.entity.Probability;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProbabilityRepository extends JpaRepository<Probability, Long> {
-}
