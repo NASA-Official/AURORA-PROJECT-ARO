1f35c493de2323f4e012826239e4e2f9d1881e0b
LeeMJ <gagopa9912@naver.com>
2023-04-01 19:33:11 +0900

    [BE] 바텀업 api 개발

--- server/aro/api-module/src/main/java/com/nassafy/api/service/ForecastService.java
+++ server/aro/api-module/src/main/java/com/nassafy/api/service/ForecastService.java
@@ -1,13 +1,18 @@
 package com.nassafy.api.service;
 
+import com.nassafy.api.dto.res.ForecastAndInterestResDTO;
 import com.nassafy.api.dto.res.ForecastResDTO;
+import com.nassafy.core.DTO.WeatherAndProbDTO;
 import com.nassafy.core.entity.Forecast;
+import com.nassafy.core.respository.AttractionRepository;
 import com.nassafy.core.respository.ForecastRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +20,8 @@ import java.time.LocalDateTime;
 public class ForecastService {
 
     private final ForecastRepository forecastRepository;
+
+    private final AttractionRepository attractionRepository;
 
     public ForecastResDTO getKP(LocalDateTime dateTime) {
         try {
@@ -26,4 +33,45 @@ public class ForecastService {
             return null;
         }
     }
+
+    public ForecastAndInterestResDTO getKpsAndInterests(LocalDateTime dateTime, Long memberId) {
+        List<Forecast> forecasts = forecastRepository.findAll();
+        int start = 0;
+        int end = 0;
+        log.info(dateTime.toString());
+
+        for (int i = forecasts.size() - 1; i >= 0; i--) {
+            if (forecasts.get(i).getDateTime().isEqual(dateTime)) {
+                if (forecasts.size() - i >= 24) {
+                    start = i;
+                    end = i + 24;
+                }
+                else {
+                    start = i - (24 - (forecasts.size() - i));
+                    end = forecasts.size();
+                }
+            }
+        }
+
+        if (start == 0 && end == 0) {
+            log.info("해당 시간은 존재하지 않습니다. ");
+            throw new IllegalArgumentException();
+        }
+
+        log.info("kp ok");
+
+        List<Float> kps = new ArrayList<>();
+
+        for (Forecast forecast: forecasts.subList(start, end)) {
+            log.info(forecast.getDateTime().toString() + " : " + forecast.getKp());
+            kps.add(forecast.getKp());
+        }
+
+        List<WeatherAndProbDTO> weatherAndProbList = attractionRepository.findWeatherAndProbList(dateTime, memberId);
+
+        return ForecastAndInterestResDTO.builder().probs(weatherAndProbList).kps(kps).build();
+    }
+
+
+
 }
