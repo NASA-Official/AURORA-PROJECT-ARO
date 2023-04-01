46779d3bbc5db085a4f016da64eed33330fb3fee
고청천 <g2c1010@naver.com>
2023-04-01 17:39:26 +0900

    [BE] Feat: 확률 데이터

--- server/aro/core-module/src/main/java/com/nassafy/core/respository/ForecastRepository.java
+++ server/aro/core-module/src/main/java/com/nassafy/core/respository/ForecastRepository.java
@@ -1,13 +1,15 @@
 package com.nassafy.core.respository;
 
 import com.nassafy.core.entity.Forecast;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface ForecastRepository extends JpaRepository<Forecast, Integer> {
 
     Optional<Forecast> findByDateTime(LocalDateTime dateTime);
-
+    List<Forecast> findAll(Sort sort);
 }
--- server/aro/core-module/src/main/java/com/nassafy/core/respository/WeatherRepository.java
+++ server/aro/core-module/src/main/java/com/nassafy/core/respository/WeatherRepository.java
@@ -2,12 +2,16 @@ package com.nassafy.core.respository;
 
 
 import com.nassafy.core.entity.Weather;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Repository
 public interface WeatherRepository extends JpaRepository<Weather, Long> {
+    List<Weather> findAll(Sort sort);
 }
