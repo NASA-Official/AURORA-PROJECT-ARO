12570d99af7e180217c03f9b2bad64252f324c79
고청천 <g2c1010@naver.com>
2023-04-04 14:51:52 +0900

    [BE] Fix: Api 수정

---
+++ server/aro/api-module/src/main/java/com/nassafy/api/dto/req/CountryIdRequest.java
@@ -0,0 +1,13 @@
+package com.nassafy.api.dto.req;
+
+public class CountryIdRequest {
+    private Long countryId;
+
+    public Long getCountryId() {
+        return countryId;
+    }
+
+    public void setCountryId(Long countryId) {
+        this.countryId = countryId;
+    }
+}
