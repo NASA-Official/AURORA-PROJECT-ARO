6ee4996c59c9874ec7f7b4cb6f80e223ef947dc7
bigyoung8375@naver.com <bigyoung8375@naver.com>
2023-04-04 16:08:47 +0900

    [FE] Feat meteor shower design modify
    Former-commit-id: 7800e6e893d0e3bb1e4ebc7a37759a69ba1807c7

--- client/Aro/app/src/main/java/com/nassafy/aro/ui/view/meteorshower/MeteorShowerFragment.kt
+++ client/Aro/app/src/main/java/com/nassafy/aro/ui/view/meteorshower/MeteorShowerFragment.kt
@@ -10,14 +10,10 @@ import androidx.recyclerview.widget.SimpleItemAnimator
 import com.nassafy.aro.R
 import com.nassafy.aro.data.dto.MeteorShower
 import com.nassafy.aro.databinding.FragmentMeteorShowerBinding
-import com.nassafy.aro.ui.adapter.DateHourSelectAdapter
 import com.nassafy.aro.ui.adapter.MeteorShowerAdapter
 import com.nassafy.aro.ui.view.BaseFragment
-import com.nassafy.aro.ui.view.dialog.MeteorCountrySelectDialog
 import com.nassafy.aro.ui.view.main.MainActivity
 import com.nassafy.aro.ui.view.main.MainActivityViewModel
-import com.nassafy.aro.util.showSnackBarMessage
-import com.nassafy.aro.util.showToastView
 
 class MeteorShowerFragment :
     BaseFragment<FragmentMeteorShowerBinding>(FragmentMeteorShowerBinding::inflate) {
@@ -59,6 +55,7 @@ class MeteorShowerFragment :
 
             binding.meteorShowerCountryTextview.visibility = View.VISIBLE
             binding.meteorShowerRecyclerview.visibility = View.VISIBLE
+            binding.altView.root.visibility = View.GONE
 
         } else {
             binding.meteorShowerCountryTextview.visibility = View.GONE
@@ -76,53 +73,52 @@ class MeteorShowerFragment :
             name = "거문고자리",
             engName = "Lyra",
             date = "2023년 4월 20일",
-            image = "https://i.pinimg.com/564x/91/d8/3c/91d83c1344a605b3b6f37052cc1e8262.jpg",
-            subImage = "https://i.pinimg.com/564x/40/26/93/402693100bf16bdc34c71ae7fb17ab21.jpg"
+            image = R.drawable.star_1,
+            subImage = R.drawable.star_2
         )
         var item2 = MeteorShower(
             name = "머리털자리",
             engName = "coma berenicids",
             date = "2023년 1월 1일",
-            image = "https://i.pinimg.com/736x/16/21/84/162184dec1a65ad53028358ffe350e6d.jpg",
-            subImage = "https://i.pinimg.com/564x/fe/48/5d/fe485d3a2edc73bc1189da2885f40a56.jpg"
+            image = R.drawable.star_2,
+            subImage = R.drawable.star_2
         )
         var item3 = MeteorShower(
             name = "거문고자리",
             engName = "Lyra",
             date = "2023년 2월 20일",
-            image = "https://i.pinimg.com/564x/91/d8/3c/91d83c1344a605b3b6f37052cc1e8262.jpg",
-            subImage = "https://i.pinimg.com/564x/40/26/93/402693100bf16bdc34c71ae7fb17ab21.jpg"
+            image = R.drawable.star_1,
+            subImage = R.drawable.star_2
         )
         var item4 = MeteorShower(
             name = "머리털자리",
             engName = "coma berenicids",
             date = "2023년 1월 1일",
-            image = "https://i.pinimg.com/736x/16/21/84/162184dec1a65ad53028358ffe350e6d.jpg",
-            subImage = "https://i.pinimg.com/564x/fe/48/5d/fe485d3a2edc73bc1189da2885f40a56.jpg"
+            image = R.drawable.star_2,
+            subImage = R.drawable.star_2
         )
         var item5 = MeteorShower(
             name = "거문고자리",
             engName = "Lyra",
             date = "2023년 2월 20일",
-            image = "https://i.pinimg.com/564x/91/d8/3c/91d83c1344a605b3b6f37052cc1e8262.jpg",
-            subImage = "https://i.pinimg.com/564x/40/26/93/402693100bf16bdc34c71ae7fb17ab21.jpg"
+            image = R.drawable.star_1,
+            subImage = R.drawable.star_1
         )
         var item6 = MeteorShower(
             name = "머리털자리",
             engName = "coma berenicids",
             date = "2023년 1월 1일",
-            image = "https://i.pinimg.com/736x/16/21/84/162184dec1a65ad53028358ffe350e6d.jpg",
-            subImage = "https://i.pinimg.com/564x/fe/48/5d/fe485d3a2edc73bc1189da2885f40a56.jpg"
+            image = R.drawable.star_2,
+            subImage = R.drawable.star_2
         )
         var item7 = MeteorShower(
             name = "거문고자리",
             engName = "Lyra",
             date = "2023년 2월 20일",
-            image = "https://i.pinimg.com/564x/91/d8/3c/91d83c1344a605b3b6f37052cc1e8262.jpg",
-            subImage = "https://i.pinimg.com/564x/40/26/93/402693100bf16bdc34c71ae7fb17ab21.jpg"
+            image = R.drawable.star_1,
+            subImage = R.drawable.star_1
         )
         var itemList = arrayListOf<MeteorShower>(item1, item2, item3, item4, item5, item6, item7)
         var countryList = arrayListOf<String>("대한민국", "아이슬란드", "가나", "일본", "중국", "미국", "영국", "프랑스")
     }
-
 }
