089131b7364a377980575b3dee61591f82670061
박채성 <97psh@naver.com>
2023-04-05 20:29:42 +0900

    [FE] Remove : Delete unused files
    Former-commit-id: c3bdf47d81bfef27e7c415c32e73d3bd019e5c2c

--- client/Aro/app/src/main/java/com/nassafy/aro/UI/TempFragment.kt
+++
@@ -1,6 +0,0 @@
-package com.nassafy.aro.ui
-
-import androidx.fragment.app.Fragment
-
-class TempFragment : Fragment() {
-}
--- client/Aro/app/src/main/java/com/nassafy/aro/data/dto/Favorite.kt
+++
@@ -1,7 +0,0 @@
-package com.nassafy.aro.data.dto
-
-data class Favorite @JvmOverloads constructor(
-   var placeId : Int? = 0,
-   var placeName : String? = "",
-   // var
-)
