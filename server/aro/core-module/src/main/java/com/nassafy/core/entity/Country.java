d3c98f96ae9cc8cd5b12e52bfc861b5a2055c5e9
怨좎껌泥 <g2c1010@naver.com>
2023-04-07 17:14:44 +0900

    [BE] Fix: 엔티티 수정
    Former-commit-id: f381cf7a5a82d3e781e3ca60d79a721ea82c31fc [formerly 86f0a34d3df67d3aa2d09f384ff373fd2bad224d]
    Former-commit-id: 3bda173f0c768409e3f54220796913b131a03020

--- server/aro/core-module/src/main/java/com/nassafy/core/entity/Country.java
+++ server/aro/core-module/src/main/java/com/nassafy/core/entity/Country.java
@@ -16,7 +16,7 @@ public class Country {
     private String country;
     private String countryEmoji;
 
-    @OneToOne(mappedBy = "country", cascade = {CascadeType.REMOVE})
+    @OneToOne(mappedBy = "country", cascade = {CascadeType.REMOVE}, orphanRemoval = true)
     private MeteorInterest meteorInterest;
 
     public Country(Long countryId, String country, String countryEmoji) {
