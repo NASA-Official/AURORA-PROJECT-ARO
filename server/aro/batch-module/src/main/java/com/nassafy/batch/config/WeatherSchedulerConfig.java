09257c6a452c8213c636a8e92ca77d0b5083386a
kcc <g2c1010@naver.com>
2023-07-18 15:27:32 +0900

    chor: @component 추가

--- server/aro/batch-module/src/main/java/com/nassafy/batch/config/WeatherSchedulerConfig.java
+++ server/aro/batch-module/src/main/java/com/nassafy/batch/config/WeatherSchedulerConfig.java
@@ -8,8 +8,10 @@ import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
+@Component
 @Configuration
 public class WeatherSchedulerConfig {
 
